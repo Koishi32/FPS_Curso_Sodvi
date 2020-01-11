d242c6d4358a080db4def30e3fa1638c015c9a5a
Diaz <alejandroivanleyvadiaz@gmail.com>
2020-01-10 22:30:53 -0600

    Paquete1
    Se termino el paquete a usar para el dia 1 lunes

--- FPS_CARDBOARD/Assets/FPS_card/scrpits/Character_move.cs
+++ FPS_CARDBOARD/Assets/FPS_card/scrpits/Character_move.cs
@@ -9,6 +9,8 @@ public class Character_move : MonoBehaviour
     bool Can_move;
     Transform camera_player;
     Vector3 direccion_camara;
+    public bool volar;
+    float y_caida; // Decidira si el objeto caera o sera manejado por el jugador
 
     // Start is called before the first frame update
     void Start() //inicializar variables
@@ -35,46 +37,31 @@ public class Character_move : MonoBehaviour
             //Asegurarse que el objeto tenga gravedad activida o volara
             controlador.velocity = new Vector3(direccion_camara.x, y_caida, direccion_camara.z) * vel; //recoje la direccion delantera de la camara y la multipica por velocidad
         }
-        else
-        {
-            controlador.velocity = new Vector3(0, y_caida, 0); //Solo permite caida en el eje y
-        }
+      
     }
 
     public void activate_move()
     {
         if (Can_move) {
+            controlador.isKinematic = true; // No sera afectado por las fisicas 
             Can_move = false;
         }
         else
-        Can_move = true;
+        {
+            Can_move = true;
+            controlador.isKinematic = false; // Sera afectado por las fisicas mientras se mueve
+        }
+      
     }
-
-    public bool volar;
-    float y_caida; // Decidira si el objeto caera o sera manejado por el jugador
-
+    //Revisa si deberia seguir la camara (volar) o seguir la gravedad
     void revisar_caida()
     {
         if (volar)
         {
             y_caida = direccion_camara.y; // vuela
         }
-        else
-        {
-            y_caida = controlador.velocity.y; // cae con gravedad pura
-        }
     }
 
-    public void Activar_vuelo()
-    {
-        if (!volar)
-        {
-            volar = true;
-            Invoke("desactiva_vuelo", 6.0f);
-        }
-    }
-    void desactiva_vuelo()
-    {
-        volar = false;
-    }
+
+
 }
