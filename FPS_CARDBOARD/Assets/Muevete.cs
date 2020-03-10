af3ce2784c2ef617689af919204d5ce5dae32fd0
Ivan <alejandroivanleyvadiaz@gmail.com>
2020-03-10 14:50:41 -0600

    VR_Box_setup
    player can now move and fire at will with the "Fire1","Fire2" and "Fire3" Buttons

--- FPS_CARDBOARD/Assets/Muevete.cs
+++ FPS_CARDBOARD/Assets/Muevete.cs
@@ -8,6 +8,15 @@ public class Muevete : MonoBehaviour
     public Rigidbody rigido;
    // public float gravedad;
     public float velocidad;
+    GvrPointerPhysicsRaycaster rayo;
+    public GameObject particulon;
+    public GameObject particulon2;
+    [SerializeField]
+    float velocidad_bala;
+    [SerializeField]
+    float offset;
+    [SerializeField]
+    float time_bullet;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +27,13 @@ public class Muevete : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        movement();
+        checa_fire();
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Application.Quit();
+        }
+    }
+    void movement() {
         float hor = Input.GetAxis("Horizontal");
         float ver = Input.GetAxis("Vertical");
         var dir = camera_mine.forward.normalized;
@@ -28,11 +44,36 @@ public class Muevete : MonoBehaviour
             //vel = vel.Normalize();
             rigido.velocity = targetDirection;
         }
-        else {
+        else
+        {
             rigido.velocity = Vector3.zero;
         }
-
-   
-
     }
-}
+    void checa_fire() {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            dispara();
+        }
+        else if (Input.GetButtonDown("Fire2") || Input.GetButtonDown("Fire3"))
+        {
+            Vector3 dir_camara = Camera.main.transform.forward.normalized * offset;
+            var clon = Instantiate(particulon2, Camera.main.transform.position + dir_camara, Camera.main.transform.rotation);
+            clon.GetComponent<Rigidbody>().AddForce(clon.transform.forward * velocidad_bala);
+            Destroy(clon.gameObject, time_bullet);
+        }
+    }
+    void dispara() {
+        /*Vector3 dir_camara = Camera.main.transform.forward.normalized;
+        var clon = Instantiate(particulon, Camera.main.transform.position + dir_camara, Camera.main.transform.rotation);
+        Destroy(clon.gameObject, 6);*/
+        if (GvrPointerInputModule.CurrentRaycastResult.gameObject != null) {
+
+
+            Vector3 Localizacion_impacto = GvrPointerInputModule.CurrentRaycastResult.gameObject.transform.position;
+            var referencia = GameObject.Instantiate(particulon, Localizacion_impacto, Quaternion.identity);
+            Destroy(referencia.gameObject, 6);
+        }
+    }
+           
+    }
+   
